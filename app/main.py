1b914b680bf7eb77800fc4502e03eb55155c09b9
Adithya Shankar <adithya7shankar@gmail.com>
2024-08-26 15:55:37 +0000

    Small increments

--- app/main.py
+++ app/main.py
@@ -11,5 +11,15 @@ file_paths = [
 # Load datasets
 datasets = {file_path.split('/')[-1]: pd.read_csv(file_path) for file_path in file_paths}
 
+print("Datasets loaded successfully!")
 # Display the datasets to the user
 datasets.keys()
+
+# Load the dataset
+df_disease_symptom = pd.read_csv("Datasets/Disease_symptom_and_patient_profile_dataset.csv")
+
+# Display the first few rows
+print(df_disease_symptom.head())
+
+# Get basic information
+print(df_disease_symptom.info())
